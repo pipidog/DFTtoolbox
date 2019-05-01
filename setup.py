45f84238b63f6d65ac88707efaa834ed48011e01
pipidog <pipidog@gmail.com>
2019-05-01 11:02:53 -0700

    update setup.py
    remove long description

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 from setuptools import setup, find_packages
 
 version='1.5'
-with open('C:/Users/pipidog/Dropbox/Code/DFTtoolbox/README.md') as file:
-    long_description = file.read()
-
 setup(
     name = 'DFTtoolbox',
     version = version,
     packages = ['DFTtoolbox'],
     description = 'A toolbox to initialize or postpocess several DFT codes',
-    long_description=long_description,
     scripts = [],
     license='MIT',
     author = 'pipidog',
